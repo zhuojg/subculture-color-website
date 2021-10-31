515e3beaca7210d8b9d4c37d22df41e9fa747934
Jing <zhuojg1519@outlook.com>
2021-10-31 23:45:07 +0800

    feature: add baidu tongji

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?${process.env.NEXT_PUBLIC_BAIDU_ANALYTICS_CODE}";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+    `,
+          }}
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
